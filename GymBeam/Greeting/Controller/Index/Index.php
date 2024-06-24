304002cd038c80488eae667fdc9725af3519a467
Ernest Kalynych <ekalinich1@gmail.com>
2024-06-25 01:44:39 +0200

    - added availability of configuration change in stores -> configuration - added configuration - showing configuration values via /casestudy

--- GymBeam/Greeting/Controller/Index/Index.php
+++ GymBeam/Greeting/Controller/Index/Index.php
@@ -1,21 +1,38 @@
 <?php
 namespace GymBeam\Greeting\Controller\Index;
 
-class Index extends \Magento\Framework\App\Action\Action
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class Index extends Action
 {
-	protected $_pageFactory;
+    protected $scopeConfig;
+    protected $path = 'GymBeam_Greeting/greeting_config_group/';
 
-	public function __construct(
-		\Magento\Framework\App\Action\Context $context,
-		\Magento\Framework\View\Result\PageFactory $pageFactory)
-	{
-		$this->_pageFactory = $pageFactory;
-		return parent::__construct($context);
-	}
+    public function __construct(
+        Context $context,
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+        parent::__construct($context);
+    }
 
-	public function execute()
-	{
-		echo "Greeting";
-		exit;
-	}
+    public function execute()
+    {
+        if ($this->isEnabled()) {
+			echo "Showing of greeting module config value is enabled. <br>";
+            echo "text: " . $this->scopeConfig->getValue($this->path . 'text', \Magento\Store\Model\ScopeInterface::SCOPE_STORE) . "<br>";
+			echo "module_config_show_enabled: " . $this->scopeConfig->getValue($this->path . 'module_config_show_enabled', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        } else {
+            echo "Showing of greeting module config value is disabled. ";
+        }
+        
+        exit;
+    }
+
+    public function isEnabled()
+    {
+        return (bool) $this->scopeConfig->getValue($this->path . 'module_config_show_enabled', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+    }
 }
